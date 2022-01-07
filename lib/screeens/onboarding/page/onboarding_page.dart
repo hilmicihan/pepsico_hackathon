6c0843beed95812630770414a2d6507904a37cd1
hilmi <hilmicihan>
2022-01-07 18:24:31 +0300

    onboardingScreen

---
+++ lib/screeens/onboarding/page/onboarding_page.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pepsico_hackathon/screeens/onboarding/bloc/onboarding_bloc.dart';
+import 'package:pepsico_hackathon/screeens/onboarding/widget/onboarding_content.dart';
+
+class OnboardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(context),
+    );
+  }
+
+  BlocProvider<OnboardingBloc> _buildBody(BuildContext context) {
+    return BlocProvider<OnboardingBloc>(
+      create: (BuildContext context) => OnboardingBloc(),
+      child: BlocConsumer<OnboardingBloc, OnboardingState>(
+        listenWhen: (_, currState) => currState is NextScreenState,
+        listener: (context, state) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (_) {
+                return Container();
+              },
+            ),
+          );
+        },
+        buildWhen: (_, currState) => currState is OnboardingInitial,
+        builder: (context, state) {
+          return OnboardingContent();
+        },
+      ),
+    );
+  }
+}
